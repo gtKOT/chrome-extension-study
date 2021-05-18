853689455c31e6f9f6fb69041b05c46957e992f6
Kota Kimura <kota-kimura@bm-sms.co.jp>
2021-05-18 20:51:38 +0900

    refactor

--- src/content_script.js
+++ src/content_script.js
@@ -1,13 +1,18 @@
+function createTextBox(text, x, y) {
+  const textBox = document.createElement("div");
+  textBox.textContent = text;
+  textBox.style.position = "absolute";
+  textBox.style.left = `${x}px`;
+  textBox.style.top = `${y}px`;
+  textBox.style.border = "1px solid";
+  textBox.style.backgroundColor = "white";
+  return textBox;
+}
+
 document.addEventListener("click", ({ pageX, pageY }) => {
   const selectedText = window.getSelection().toString();
 
-  const textBox = document.createElement("div");
-  textBox.textContent = selectedText;
-  textBox.style.position = "absolute";
-  textBox.style.top = `${pageY}px`;
-  textBox.style.left = `${pageX}px`;
-  textBox.style.border = "1px solid";
-  textBox.style.backgroundColor = "white";
+  const textBox = createTextBox(selectedText, pageX, pageY);
 
   document.body.appendChild(textBox);
 });
