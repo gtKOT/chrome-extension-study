017323218d7d889ee7ed7e6a4eb0f0d8968309ad
Kota Kimura <kota-kimura@bm-sms.co.jp>
2021-05-18 20:47:28 +0900

    選択した文字列を選択した場所にdivで表示

--- src/content_script.js
+++ src/content_script.js
@@ -1,11 +1,11 @@
-document.addEventListener("click", () => {
+document.addEventListener("click", ({ pageX, pageY }) => {
   const selectedText = window.getSelection().toString();
 
   const textBox = document.createElement("div");
   textBox.textContent = selectedText;
   textBox.style.position = "absolute";
-  textBox.style.top = "100px";
-  textBox.style.left = "100px";
+  textBox.style.top = `${pageY}px`;
+  textBox.style.left = `${pageX}px`;
   textBox.style.border = "1px solid";
   textBox.style.backgroundColor = "white";
 
