a3e49cfabb0394d52c26e7e017497ab1f161630b
Kota Kimura <kota-kimura@bm-sms.co.jp>
2021-05-18 20:45:23 +0900

    選択した文字列をdivで表示

--- src/content_script.js
+++ src/content_script.js
@@ -1,4 +1,13 @@
 document.addEventListener("click", () => {
   const selectedText = window.getSelection().toString();
-  window.alert(selectedText);
+
+  const textBox = document.createElement("div");
+  textBox.textContent = selectedText;
+  textBox.style.position = "absolute";
+  textBox.style.top = "100px";
+  textBox.style.left = "100px";
+  textBox.style.border = "1px solid";
+  textBox.style.backgroundColor = "white";
+
+  document.body.appendChild(textBox);
 });
